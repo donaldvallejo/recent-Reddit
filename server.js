9f6dce2dee1e970f6354567ad7e9f8df2c049802
donald vallejo <donny.vallejo@gmail.com>
2020-06-11 23:05:29 -0700

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const app = express();
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const app = express();
 const port = 3000
 
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+require('./data/reddit-db');
+require('./controllers/posts.js')(app);
+
+app.use(expressValidator());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
