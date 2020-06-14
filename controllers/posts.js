201bd73ab6c8e5c9923e8645e16094b7e2b2ba4e
donald vallejo <donny.vallejo@gmail.com>
2020-06-14 16:17:52 -0700

    added another route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,28 @@
 const Post = require('../models/post');
+const Post2 = require('../models/post2');
 
 module.exports = (app) => {
 
   // CREATE
   app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
     const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
     post.save((err, post) => {
+      // REDIRECT TO THE ROOT
       return res.redirect(`/`);
+    })
+  });
+  
+  app.post('/posts2/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post2(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/posts2/new`);
     })
   });
 
